51c9f2eb40eb3107fc4b6dd7bf74da5d8e7f3211
ted7088 <ted3131@naver.com>
2024-03-12 21:22:53 +0900

    commit

--- src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -46,6 +46,7 @@ public class LoginCheckFilter implements Filter {
     /**
      * 화이트 리스트의 경우 인증 체크X
      */
+
     private boolean isLoginCheckPath(String requestURI) {
         return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
     }
